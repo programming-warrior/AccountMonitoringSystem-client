135f824aac9c0006fa79697f8d96bfd6db1a4a83
Yusuf <mdyusuf2521@gmail.com>
2024-05-04 19:37:04 +0530

    Styled the app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/login">Login</Link>
-      <Link href="/register">Register</Link>
+    <main className="flex min-h-screen  justify-between p-24">
+      <Link className="border border-gray-400 w-fit h-fit py-2 px-4 rounded hover:bg-black hover:text-white transition-all"href="/login">Login</Link>
+      <Link className="border border-gray-400 w-fit h-fit py-2 px-4 rounded transition-all  hover:bg-black hover:text-white" href="/register">Register</Link>
     </main> 
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -46,7 +46,7 @@ const RegisterPage = () => {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
+              Create a new Account
             </h1>
             <form className="space-y-4 md:space-y-6" action="#" onSubmit={(e)=>{handleSubmit(e)}}>
               <div>
@@ -62,7 +62,7 @@ const RegisterPage = () => {
               </div>
            
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                already have an account yet? <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                already have an account yet? <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Log in </Link>
               </p>
             </form>
           </div>
