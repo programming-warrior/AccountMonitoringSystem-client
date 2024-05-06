4a0280dec2b09429f6bc0fc593f557def39050a2
Yusuf Azam <104256811+programming-warrior@users.noreply.github.com>
2024-05-06 16:20:07 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        NEXT_BACKEND_URL:'http://localhost:8000',
-        NEXT_PUBLIC_BACKEND_WS:'ws://localhost:8000'
+        NEXT_BACKEND_URL:'https://accountmonitoringsystem-backend.onrender.com',
+        NEXT_PUBLIC_BACKEND_WS:'wss://accountmonitoringsystem-backend.onrender.com'
     }
 };
 
